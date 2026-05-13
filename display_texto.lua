136c062b94f1a15d2638ace95dbb7037cd9103db
ethanprogra1116 <138069624+ethanprogra1116@users.noreply.github.com>
2026-05-12 18:55:59 -0600

    Agregamos mas imagenes de experimentacion y las agregamos a openvibe, el lua ya muestra el procedimiento experimental completo

--- display_texto.lua
+++ display_texto.lua
@@ -1,32 +1,42 @@
-function initialize(box)
+function initialize(box) -- initialize tiene que ir con ese nombre exacto en OpenVibe.
   dofile(box:get_config("${Path_Data}") .. "/plugins/stimulation/lua-stimulator-stim-codes.lua")
 end
 
-function process(box)
+function process(box) -- process tiene que ir con ese nombre exacto en OpenVibe.
   local t
 
-  -- Imagen 1: fondo negro aparece
+  -- Fase 1: 2 min ojos cerrados
   t = box:get_current_time()
   box:send_stimulation(1, OVTK_StimulationId_Label_00, t, 0)
-
-  -- Espera 3 segundos
-  while box:get_current_time() < t + 60 do
+  while box:get_current_time() < t + 120 do
     box:sleep()
   end
 
-  -- Imagen 2: texto aparece
+  -- Fase 2: 2 min ojos abiertos
+  t = box:get_current_time()
+  box:send_stimulation(1, OVTK_StimulationId_Label_01, t, 0)
+  while box:get_current_time() < t + 120 do
+    box:sleep()
+  end
+
+  -- Fase 3: 3 min lectura
   t = box:get_current_time()
   box:send_stimulation(1, OVTK_StimulationId_Label_02, t, 0)
-
-  -- Espera 3 segundos
   while box:get_current_time() < t + 180 do
+    box:sleep()
+  end
+
+  -- Fase 4: 5 min explicación
+  t = box:get_current_time()
+  box:send_stimulation(1, OVTK_StimulationId_Label_03, t, 0)
+  while box:get_current_time() < t + 300 do
     box:sleep()
   end
 
   -- Detiene el escenario
   t = box:get_current_time()
-  box:send_stimulation(1, OVTK_StimulationId_Label_03, t, 0)
+  box:send_stimulation(1, OVTK_StimulationId_Label_04, t, 0)
 end
 
-function uninitialize(box)
+function uninitialize(box) -- uninitialize tiene que ir con ese nombre exacto en OpenVibe.
 end
